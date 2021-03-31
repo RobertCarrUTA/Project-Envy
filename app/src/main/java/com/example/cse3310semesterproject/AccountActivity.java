3df39190cdb40b23955005b944d877737951017d
RobertCarrUTA <robertcarrcisd@gmail.com>
2021-03-31 04:34:45 -0500

    Second push, not much changed. Made slight changes and also made a password change feature that saves into nothing, but the ground work is there.

--- app/src/main/java/com/example/cse3310semesterproject/AccountActivity.java
+++ app/src/main/java/com/example/cse3310semesterproject/AccountActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AccountActivity extends AppCompatActivity
 {
-    Button mReturnHomeBtn, mSignOutFromAccountBtn;
+    EditText mNewCurrentPasswordEntry;
+    Button mReturnHomeBtn, mSignOutFromAccountBtn, mChangePasswordBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +23,8 @@ public class AccountActivity extends AppCompatActivity
 
         mReturnHomeBtn = findViewById(R.id.ReturnHomeBtn);
         mSignOutFromAccountBtn = findViewById(R.id.SignOutFromAccountBtn);
+        mChangePasswordBtn = findViewById(R.id.changePasswordBtn);
+        mNewCurrentPasswordEntry = findViewById(R.id.newCurrentPasswordEntry);
 
         // Let the user go back to the homepage
         mReturnHomeBtn.setOnClickListener(new View.OnClickListener()
@@ -40,6 +44,39 @@ public class AccountActivity extends AppCompatActivity
             {
                 Toast.makeText(AccountActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+
+        // This is for the password change functionality, needs database check I guess
+        mChangePasswordBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String passwordValue = mNewCurrentPasswordEntry.getText().toString().trim();
+
+                // If the password text entry is empty tell the user that the password is required
+                if(TextUtils.isEmpty(passwordValue))
+                {
+                    mNewCurrentPasswordEntry.setError("Password is required");
+                    return;
+                }
+
+                // We should make the 6 in this case an int value we define and change
+                // once then it changes through the whole program
+                if(passwordValue.length() < 6)
+                {
+                    mNewCurrentPasswordEntry.setError("Password should be more than 6 characters");
+                    return;
+                }
+
+                // This if condition is the one that determine if the login information was correct and tells the user
+                // the login was successful then takes them to the homepage (MainActivity)
+                if(passwordValue.length() > 6)
+                {
+                    Toast.makeText(AccountActivity.this, "Password changed successfully!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
