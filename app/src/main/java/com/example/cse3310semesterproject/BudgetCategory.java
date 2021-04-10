a0ea2e4726ac461285ef8412c41750d2c488b7aa
RobertCarrUTA <robertcarrcisd@gmail.com>
2021-04-10 14:27:12 -0500

    Got the budget categories to be added from user created titles, although the process needs to be made better.

--- app/src/main/java/com/example/cse3310semesterproject/BudgetCategory.java
+++ app/src/main/java/com/example/cse3310semesterproject/BudgetCategory.java
@@ -1,13 +1,11 @@
 package com.example.cse3310semesterproject;
 
-public class BudgetCategory { //
-    // Should we make these private?
+public class BudgetCategory {
     public String userID;
     public String categoryTitle;
     public String priorityString;
     public int priorityInt;
 
-    // We need an empty constructor when using our database
     public BudgetCategory(){}
 
     public BudgetCategory(String userID, String categoryTitle, String priorityString, int priorityInt)
@@ -17,51 +15,4 @@ public class BudgetCategory { //
         this.priorityString = priorityString;
         this.priorityInt = priorityInt;
     }
-
-    ///////////////////////////////////////////////////////
-    // We need getters and setters for our data
-    public String getUserID()
-    {
-        return userID;
-    }
-
-    public void setUserID(String userID)
-    {
-        this.userID = userID;
-    }
-
-
-    ///////////////////////////////////////////////////////
-    public String getCategoryTitle()
-    {
-        return categoryTitle;
-    }
-
-    public void setCategoryTitle(String categoryTitle)
-    {
-        this.categoryTitle = categoryTitle;
-    }
-
-    ///////////////////////////////////////////////////////
-    public String getPriorityString()
-    {
-        return priorityString;
-    }
-
-    public void setPriorityString(String priorityString)
-    {
-        this.priorityString = priorityString;
-    }
-
-    ///////////////////////////////////////////////////////
-    public int getPriorityInt()
-    {
-        return priorityInt;
-    }
-
-    public void setPriorityString(int priorityInt)
-    {
-        this.priorityInt = priorityInt;
-    }
-
 }
