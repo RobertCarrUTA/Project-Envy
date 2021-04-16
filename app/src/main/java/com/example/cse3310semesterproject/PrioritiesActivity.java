048a0ccfacce5325996e8d3a3957863f23c32459
Jaedyn Brown <jaedyn_brown@yahoo.com>
2021-04-16 16:25:52 -0500

    Pulled Michael's code, readjusted Priority Activity pushes

--- app/src/main/java/com/example/cse3310semesterproject/PrioritiesActivity.java
+++ app/src/main/java/com/example/cse3310semesterproject/PrioritiesActivity.java
@@ -118,19 +118,19 @@ public class PrioritiesActivity extends AppCompatActivity implements AdapterView
                     {
                         case 0: // If High is selected on the drop down menu
                             BudgetCategory budgetCategory = new BudgetCategory(uid, CategoryTitle, "High", 1);
-                            FirebaseDatabase.getInstance().getReference("Users").child(uid).push().child("Budget Category").setValue(budgetCategory);
+                            FirebaseDatabase.getInstance().getReference("Users").child(uid).child("Budget Category").push().setValue(budgetCategory);
                             mCategoryTitleEntry.getText().clear();
                             Toast.makeText(PrioritiesActivity.this, "Category " + CategoryTitle + " created successfully!", Toast.LENGTH_SHORT).show();
                             break;
                         case 1: // If Medium is selected on the drop down menu
                             BudgetCategory budgetCategory2 = new BudgetCategory(uid, CategoryTitle, "Medium", 2);
-                            FirebaseDatabase.getInstance().getReference("Users").child(uid).push().child("Budget Category").setValue(budgetCategory2);
+                            FirebaseDatabase.getInstance().getReference("Users").child(uid).child("Budget Category").push().setValue(budgetCategory2);
                             mCategoryTitleEntry.getText().clear();
                             Toast.makeText(PrioritiesActivity.this, "Category " + CategoryTitle + " created successfully!", Toast.LENGTH_SHORT).show();
                             break;
                         case 2: // If Low is selected on the drop down menu
                             BudgetCategory budgetCategory3 = new BudgetCategory(uid, CategoryTitle, "Low", 3);
-                            FirebaseDatabase.getInstance().getReference("Users").child(uid).push().child("Budget Category").setValue(budgetCategory3);
+                            FirebaseDatabase.getInstance().getReference("Users").child(uid).child("Budget Category").push().setValue(budgetCategory3);
                             mCategoryTitleEntry.getText().clear();
                             Toast.makeText(PrioritiesActivity.this, "Category " + CategoryTitle + " created successfully!", Toast.LENGTH_SHORT).show();
                             break;
