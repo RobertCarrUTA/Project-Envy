c2f198e41d3c3cbbce95fe16d3a119a3388ea2a7
Mike Stokes <cse3310group1@gmail.com>
2021-04-21 18:07:59 -0500

    Testing BudgetAnalysisActivity page. Looks filled out but needs a test. If this method works, it can be applied to WeeklyReportActivity as well.

--- app/src/main/java/com/example/cse3310semesterproject/BudgetAnalysisActivity.java
+++ app/src/main/java/com/example/cse3310semesterproject/BudgetAnalysisActivity.java
@@ -1,9 +1,12 @@
 package com.example.cse3310semesterproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,9 +17,21 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.DateFormat;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Currency;
+import java.util.Date;
 import java.util.Locale;
 
 public class BudgetAnalysisActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
@@ -30,15 +45,26 @@ public class BudgetAnalysisActivity extends AppCompatActivity implements Adapter
 
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     String uid = user.getUid();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reff = database.getReference().child("Users").child(uid).child("Expenses");
+    DatabaseReference totReff = database.getReference().child("Users").child(uid).child("Budgets");
 
     private Spinner spinner;
-    private static final String[] paths = {"High Priority Spending", "Medium Priority Spending", "Low Priority Spending"};
+    private static final String[] paths = {"Select Priority Level", "High Priority Spending", "Medium Priority Spending", "Low Priority Spending"};
+    static double lowTot = 0, medTot = 0, highTot = 0, budget = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_analysis);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_WEEK, -(calendar.get(Calendar.DAY_OF_WEEK)-2));
+        Date monDate = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_WEEK, 7);
+        Date nextMonDate = calendar.getTime();
+        System.out.println(monDate);
 
         mHowMuchSpentTextBox = findViewById(R.id.howMuchSpentTextBox);
         mPercentageTextBox = findViewById(R.id.percentageTextBox);
@@ -56,7 +82,60 @@ public class BudgetAnalysisActivity extends AppCompatActivity implements Adapter
          */
 
         mBelowOrAboveBudgetTextBox.setText("You are still within your budget!");
+        //------------------------------------------------------------------------------------------
 
+        totReff.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                Budgets budgetObj = dataSnapshot.getValue(Budgets.class);
+                budget = budgetObj.budget;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        reff.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren())
+                {
+                    Expenses expenses = snapshot.getValue(Expenses.class);
+                    if(expenses.creationDate.compareTo(nextMonDate) <= 0 && expenses.creationDate.compareTo(monDate) >= 0)
+                    {
+                        if(expenses.priority == 1)
+                        {
+                            highTot = highTot + expenses.spending;
+                        }
+                        else if(expenses.priority == 2)
+                        {
+                            medTot = medTot + expenses.spending;
+                        }
+                        else if(expenses.priority == 3)
+                        {
+                            lowTot = lowTot + expenses.spending;
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+                // Failed to read value
+                Log.v("TestRead", "Failed to read value.", databaseError.toException());
+            }
+        });
+
+
+        //------------------------------------------------------------------------------------------
         Spinner spinner = (Spinner) findViewById(R.id.prioritySelection_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         // The priority_array was made in strings.xml
@@ -67,6 +146,7 @@ public class BudgetAnalysisActivity extends AppCompatActivity implements Adapter
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
 
 
         //------------------------------------------------------------------------------------------
@@ -102,26 +182,28 @@ public class BudgetAnalysisActivity extends AppCompatActivity implements Adapter
 
         switch (position)
         {
-            case 0: // High
+            case 0:
+                break;
+            case 1: // High
                 // need to be set to the total expenses for high priority for the current week
-                mHowMuchSpentTextBox.setText(dollarFormat.format(310));
-                percentage = (310.0 / 680.0) * 100.0;
+                mHowMuchSpentTextBox.setText(dollarFormat.format(highTot));
+                percentage = (highTot / budget) * 100.0;
                 PercentageString = String.format("%.2f", percentage);
                 percentage = Double.valueOf(PercentageString);
                 mPercentageTextBox.setText("%" + percentage);
                 break;
-            case 1: // Medium
+            case 2: // Medium
                 // need to be set to the total expenses for medium priority for the current week
-                mHowMuchSpentTextBox.setText(dollarFormat.format(220));
-                percentage = (220.0 / 680.0) * 100.0;
+                mHowMuchSpentTextBox.setText(dollarFormat.format(medTot));
+                percentage = (medTot / budget) * 100.0;
                 PercentageString = String.format("%.2f", percentage);
                 percentage = Double.valueOf(PercentageString);
                 mPercentageTextBox.setText("%" + percentage);
                 break;
-            case 2: // Low
+            case 3: // Low
                 // need to be set to the total expenses for low priority for the current week
-                mHowMuchSpentTextBox.setText(dollarFormat.format(150));
-                percentage = (150.0 / 680.0) * 100.0;
+                mHowMuchSpentTextBox.setText(dollarFormat.format(lowTot));
+                percentage = (lowTot / budget) * 100.0;
                 PercentageString = String.format("%.2f", percentage);
                 percentage = Double.valueOf(PercentageString);
                 mPercentageTextBox.setText("%" + percentage);
